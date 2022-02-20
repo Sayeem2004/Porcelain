acf75d3e79176b1b645f5bafc33972d6f8b00b17
Sayeem2004 <MohammadSSKC@gmail.com>
2022-02-20 15:44:02 -0500

    restructure

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 all:
 	python3 src/main.py
+
+clean:
+	rm -rf src/__pycache__
--- src/main.py
+++ src/main.py
@@ -4,6 +4,18 @@ import draw
 import util
 import var
 
+# Function that binds keystrokes to commands
+def bind_keystrokes():
+    # Basic drawing
+    var.canvas.bind("<Button-1>", draw.get_coords)
+    var.canvas.bind("<B1-Motion>", draw.draw_line)
+
+    # Changing window size
+    var.app.bind("<Right>", util.increase_width)
+    var.app.bind("<Left>", util.decrease_width)
+    var.app.bind("<Down>", util.increase_height)
+    var.app.bind("<Up>", util.decrease_height)
+
 # Main function
 def main():
     # Creating app screen
@@ -15,12 +27,7 @@ def main():
     var.canvas.pack(anchor='nw', fill='both', expand=1)
 
     # Binding Keystrokes
-    var.canvas.bind("<Button-1>", draw.get_coords)
-    var.canvas.bind("<B1-Motion>", draw.draw_line)
-    var.app.bind("<Right>", util.increase_width)
-    var.app.bind("<Left>", util.decrease_width)
-    var.app.bind("<Down>", util.increase_height)
-    var.app.bind("<Up>", util.decrease_height)
+    bind_keystrokes()
 
     # Looping
     var.app.mainloop()
